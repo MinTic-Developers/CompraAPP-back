e34d59846434e8bfea9aa9b25126a93bc403b052
nguerrero699 <nguerrero699@gmail.com>
2020-12-18 11:27:46 -0500

    listo heroku

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
-    "http://localhost", "http://localhost:8082",
+    "https://compraapp-front.herokuapp.com/",
 ]
 
 app.add_middleware(
